d4ed4a4df442f360530b7be733034934d5e465a0
BrizaMD <late16@gmail.com>
2021-08-22 17:20:55 +0200

    [added] basic fetch, need to refactor

--- src/App.js
+++ src/App.js
@@ -1,26 +1,78 @@
-import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/navbar';
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import React from "react";
+import axios from 'axios';
+
+class Pokemon extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {name: props.name, detailsUrl: props.url, sprite: ''};
+    }
+
+    componentDidMount() {
+        axios.get(this.state.detailsUrl)
+            .then(res => {
+                const temp = res.data;
+                this.setState({sprite: temp.sprites.front_default});
+            })
+    }
+
+    render() {
+        return <img src={this.state.sprite} alt={'image of ' + this.state.name}/>;
+    }
+}
+
+class Pokemons extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {fullResult: [], pokemons: []};
+    }
+
+    componentDidMount() {
+        axios.get('https://pokeapi.co/api/v2/pokemon')
+            .then(res => {
+                const temp = res.data;
+                this.setState({fullResult: temp, pokemons: temp.results});
+            })
+    }
+
+    render() {
+        console.log(this.state.pokemons);
+        let elements = this.state.pokemons.map(p => <Pokemon url={p.url} name={p.name}/>);
+        // <div className='gridItem'>Name: {p.name} <br/> Url: {p.url} </div>
+        return <div id='gridContainer'>{elements}</div>;
+    }
+}
+
+
+
+function Home() {
+    return <h1>Welcome to Reactomon Ultimate!</h1>;
+}
+
+function Types() {
+    return <h1>List by Types</h1>;
+}
 
 function App() {
   return (
 
     <div className="App">
-      <Navbar/>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Home/>
+        <Router>
+            <Navbar/>
+            <Switch>
+                <Route path="/pokemons">
+                    <Pokemons />
+                </Route>
+                <Route path="/types">
+                    <Types />
+                </Route>
+            </Switch>
+        </Router>
+
+
     </div>
   );
 }
