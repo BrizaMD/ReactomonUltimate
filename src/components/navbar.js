1803cca0044fe7b133eec55b93144873aa210e5b
BrizaMD <late16@gmail.com>
2021-08-28 16:21:08 +0200

    [removed] navbar css and converted to styled component

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,19 +1,31 @@
-import './navbar.css';
 import React from "react";
 import {
     Link
 } from "react-router-dom";
-
+import styled from "styled-components";
 
 const Navbar = () => {
     return(
-        <div className="navbar">
+        <NavBarStyle className="navbar">
             <ul>
                 <li><Link to="/pokemons">Pokemons</Link></li>
                 <li><Link to="/types">Types</Link></li>
             </ul>
-        </div>
+        </NavBarStyle>
     )
 }
 
 export default Navbar;
+
+const NavBarStyle = styled.div`
+  ul{
+    list-style-type: none;
+    margin: 5px;
+    padding: 5px;
+  }
+  li {
+    font-size: 20px;
+    display: inline;
+    margin: 10px;
+  }
+`;
