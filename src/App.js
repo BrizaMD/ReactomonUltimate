44bbacaad7ed6ae79498db5f1dd0f3dec4fd16f9
BrizaMD <late16@gmail.com>
2021-08-29 16:17:52 +0200

    [added] basic theme switcher

--- src/App.js
+++ src/App.js
@@ -1,29 +1,40 @@
 import './App.css';
 import Navbar from './components/navbar';
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
-import React from "react";
+import React, {useState, useContext} from "react";
 import PokemonsList from "./components/PokemonsList";
 import Home from "./components/Home";
 import Types from "./components/Types";
 import PokemonDetail from "./components/PokemonDetail";
+import { lightTheme, darkTheme, GlobalStyles } from "./context/Theme.js";
+import {ThemeProvider} from "styled-components";
 
 const App = () => {
+    const [theme, setTheme] = useState('dark');
+    const themeToggler = () => {
+        theme === 'light' ? setTheme('dark') : setTheme('light')
+    }
+
   return (
-    <div className="App">
-        <Home/>
-        <Router>
-            <Navbar/>
-            <Switch>
-                <Route title="Reactomon Ultimate" path="/pokemons" exact>
-                    <PokemonsList />
-                </Route>
-                <Route title="Reactomon Ultimate" path="/pokemons/:id" render={(props) => <PokemonDetail {...props} />} exact />
-                <Route title="Reactomon Ultimate" path="/types" exact>
-                    <Types />
-                </Route>
-            </Switch>
-        </Router>
-    </div>
+      <ThemeProvider theme={theme === "dark" ? darkTheme : lightTheme}>
+          <GlobalStyles />
+        <div className="App">
+            <button onClick={themeToggler}>Switch Theme</button>
+            <Home theme={theme} />
+            <Router>
+                <Navbar theme={theme} setTheme={setTheme} />
+                <Switch>
+                    <Route title="Reactomon Ultimate" path="/pokemons" theme={theme} exact>
+                        <PokemonsList theme={theme}  />
+                    </Route>
+                    <Route title="Reactomon Ultimate" path="/pokemons/:id" render={(props) => <PokemonDetail {...props} theme={theme}  />} exact />
+                    <Route title="Reactomon Ultimate" path="/types" theme={theme}  exact>
+                        <Types theme={theme}  />
+                    </Route>
+                </Switch>
+            </Router>
+        </div>
+      </ThemeProvider>
   );
 }
 
